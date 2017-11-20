3382586428e7ead5462fd6f16b4b45aa9fcb001a
Edwin Groothuis <edwin@mavetju.org>
2017-11-20 13:43:39 +1100

    Initial Commit

---
+++ luatest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  luatest
+//
+//  Created by Edwin Groothuis on 20/11/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
