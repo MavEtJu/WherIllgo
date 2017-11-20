f222b16f648eba46259ec39c0e101cc9d562f803
Edwin Groothuis <edwin@mavetju.org>
2017-11-20 13:59:54 +1100

    Add LuaContext

--- luatest/main.m
+++ luatest/main.m
@@ -7,6 +7,44 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LuaContext.h"
+
+static NSString *const myScript =
+LUA_STRING(
+           globalVar = { 0.0, 1.0 }
+
+           function myFunction(parameter)
+           return parameter >= globalVar[1] and parameter <= globalVar[2]
+           end
+           );
+
+void doLua(void)
+{
+    LuaContext *ctx = [LuaContext new];
+    NSError *error = nil;
+    if( ! [ctx parse:myScript error:&error] ) {
+        NSLog(@"Error parsing lua script: %@", error);
+        return;
+    }
+
+    NSLog(@"globalVar is: %@", ctx[@"globalVar"]); // should print "globalVar is: [ 0.0, 1.0 ]"
+
+    id result = [ctx call:@"myFunction" args:@[ @0.5 ] error:&error];
+    if( error ) {
+        NSLog(@"Error calling myFunction: %@", error);
+        return;
+    }
+    NSLog(@"myFunction returned: %@", result); // should print "myFunction returned: '1'"
+
+    ctx[@"globalVar"] = @[ 0.2, 0.4 ];
+
+    result = [ctx call:@"myFunction" args:@[ @0.5 ] error:&error];
+    if( error ) {
+        NSLog(@"Error calling myFunction: %@", error);
+        return;
+    }
+    NSLog(@"myFunction returned: %@", result); // should print "myFunction returned: '0'"
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
