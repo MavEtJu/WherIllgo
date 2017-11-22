6170d28e4369b96ab5f6f638d9552915fb796012
Edwin Groothuis <edwin@mavetju.org>
2017-11-22 22:06:46 +1100

    Put all viewcontrollers inside navigation controllers.

--- ios/AppDelegate.m
+++ ios/AppDelegate.m
@@ -25,24 +25,35 @@
     UITabBarController *tbc = [[UITabBarController alloc] init];
 
     locationsViewController = [[LocationsViewController alloc] init];
-    locationsViewController.title = @"Locations";
+    locationsViewController.title = @"LLL";
+    UINavigationController *locationsNav = [[UINavigationController alloc] initWithRootViewController:locationsViewController];
+//    locationsNav.navigationBarHidden = YES;
+
     inventoryViewController = [[InventoryViewController alloc] init];
     inventoryViewController.title = @"Inventory";
+    UINavigationController *inventoryNav = [[UINavigationController alloc] initWithRootViewController:inventoryViewController];
+
     youSeeViewController = [[YouSeeViewController alloc] init];
     youSeeViewController.title = @"You See";
+    UINavigationController *youSeeNav = [[UINavigationController alloc] initWithRootViewController:youSeeViewController];
+
     tasksViewController = [[TasksViewController alloc] init];
     tasksViewController.title = @"Tasks";
+    UINavigationController *tasksNav = [[UINavigationController alloc] initWithRootViewController:tasksViewController];
+
     mapViewController = [[MapViewController alloc] init];
     mapViewController.title = @"Maps";
+    UINavigationController *mapNav = [[UINavigationController alloc] initWithRootViewController:mapViewController];
+
 
     NSArray *controllers = @[
-        locationsViewController,
-        tasksViewController,
-        youSeeViewController,
-        inventoryViewController,
-        mapViewController
+//        locationsViewController,
+        locationsNav,
+        inventoryNav,
+        youSeeNav,
+        tasksNav,
+        mapNav
     ];
-
     [tbc setViewControllers:controllers animated:YES];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
